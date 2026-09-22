f70dff047b0fb4948c47485dc6de5642e55d6ca0
lukif96-source <lukif96@gmail.com>
2026-09-22 11:14:46 +0200

    Delete js directory

--- js/sw-registrierung.js
+++
@@ -1,3 +0,0 @@
-if('serviceWorker' in navigator && (location.protocol === 'https:' || location.hostname === 'localhost' || location.hostname === '127.0.0.1')){
-  window.addEventListener('load', () => { navigator.serviceWorker.register('sw.js').catch(e => console.warn('Service Worker:', e)); });
-}
